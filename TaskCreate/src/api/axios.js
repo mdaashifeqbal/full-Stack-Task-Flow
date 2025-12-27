5c498b897b968cb771922f9aff507df869bfbaf4
aashu874 <mdaashif001112@gmail.com>
2025-12-27 20:56:27 +0530

    minor changes

--- TaskCreate/src/api/axios.js
+++ TaskCreate/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://my-flow-backend.onrender.com/",
+  baseURL: "https://my-flow-backend.onrender.com/api",
   withCredentials: true,
 });
 
